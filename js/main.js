f230345fce7df4545c21d9f8c38b70207ef5f835
unknown <jorge.almiron@GLB-D0570.globant.com>
2014-04-30 10:26:31 -0300

    main.js editado para POO

--- js/main.js
+++ js/main.js
@@ -1,24 +1,49 @@
 /*
  * Diego Almirón
- * localStorage con jQuery
- *
+ * localStorage con jQuery (POO)
+ */
 
 $(function() {
 	/*
-	 * Guardar los datos en local storage
-	 * y obtenerlos
-	 *
-	var save = $('#save'); // document.getElementById('save'),
-		nombre = $('#nombre'); // document.getElementById('nombre'),
-		direccion = $('#direccion'); // document.getElementById('direccion'),
-		email = $('#email'); // document.getElementById('email');
+	 * Persona Object
+	 */
+	function Persona(nombre, direccion, email) {
+		this.nombre = nombre;
+		this.direccion = direccion;
+		this.email = email;
 
-	$(save).click(function() {
-		localStorage.setItem('nombre', nombre.val()); // nombre.value
-		localStorage.setItem('direccion', direccion.val()); // direccion.value
-		localStorage.setItem('email', email.val()); // email.value
-	});
+		/*
+		 * Function: save
+		 */
+		this.guardar = function() {
+			localStorage.setItem('nombre', this.nombre.val());
+			localStorage.setItem('direccion', this.direccion.val());
+			localStorage.setItem('email', this.email.val());
+			return console.log('persona guardada');
+		}
 
+		/*
+		 * Function: edit
+		 */
+		this.editar = function() {
+			localStorage.setItem('nombre', this.nombre.val());
+			localStorage.setItem('direccion', this.direccion.val());
+			localStorage.setItem('email', this.email.val());
+			window.alert("Se editaron los datos del localStorage.");
+		}
+
+		/*
+		 * FUnction: delete
+		 */
+		this.borrar = function() {
+			localStorage.clear();
+	 		window.alert("Se eliminaron los datos del localStorage.");
+		}
+	}
+
+	/*
+	 * FUnction: display data if exists
+	 */
 	if (localStorage.getItem('nombre')) {
 		var nombreg = $('#nombreg'); // document.getElementById('nombreg'),
 			direcciong = $('#direcciong'); // document.getElementById('direcciong'),
@@ -30,56 +55,37 @@ $(function() {
 	};
 
 	/*
-	 * Editar los datos existentes
-	 *
-	var edit =  $('#edit'); // document.getElementById('edit'),
-		nombreg = $('#nombreg'); // document.getElementById('nombreg'),
-		direcciong = $('#direcciong'); // document.getElementById('direcciong'),
-		emailg = $('#emailg'); // document.getElementById('emailg');
+	 * Mouse jQuery click events: save, edit, delete
+	 */
 
-	$(edit).click(function() {
-		localStorage.setItem('nombre', nombreg.val()); // nombreg.value
-		localStorage.setItem('direccion', direcciong.val()); // direcciong.value
-		localStorage.setItem('email', emailg.val()); // emailg.value
-		window.alert("Se han editados los datos del localStorage.");
+	var save = $('#save'),
+		edit = $('#edit'),
+		borrar = $('#borrar');
+
+	$(save).click(function() {
+		var nombre = $('#nombre'),
+			direccion = $('#direccion'),
+			email = $('#email');
+
+		var p = new Persona(nombre, direccion, email);
+		p.guardar();
 	});
 
-	/*
-	 * Eliminar
-	 *
-	 var borrar = document.getElementById('borrar');
+	$(edit).click(function() {
+		var nombre = $('#nombreg'),
+			direccion = $('#direcciong'),
+			email = $('#emailg');
 
-	 $(borrar).click(function() {
-	 	localStorage.clear();
-	 	window.alert("Se eliminaron los datos del localStorage.");
-	 });
-}); */
+		var p = new Persona(nombre, direccion, email);
+		p.editar();
+	});
 
-/*
- * Diego Almirón
- * localStorage con jQuery (POO)
- */
+	$(borrar).click(function() {
+		// var nombre = $('#nombreg'),
+		// 	direccion = $('#direcciong'),
+		// 	email = $('#emailg');
 
-$(function() {
-	/*
-	 * Objeto persona
-	 */
-	function Persona(nombre, direccion, email) {
-		$this.nombre = nombre;
-		$this.direccion = direccion;
-		$this.email = email;
-
-		$this.guardar = function() {
-			localStorage.setItem('nombre', $this.nombre.val());
-			localStorage.setItem('direccion', $this.direccion.val());
-			localStorage.setItem('email', $this.email.val());
-			return console.log('persona guardada');
-		}
-	}
-
-	var save = $('#save');
-	$(save).click(function() {
-		var p = new Persona($('#nombre'), $('#direccion'), $('#email'));
-		p.guardar();
+		var p = new Persona();
+		p.borrar();
 	});
 });
